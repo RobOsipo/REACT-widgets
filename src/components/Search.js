b0cd357eb6e2d89cb4d0ca0f9870378fc2c3367f
Robert <osipovitchr@gmail.com>
2022-03-29 11:16:20 -0400

    axios call + useEffect + params

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 
 
 const Search = () => {
 
-    const [term, setTerm] = React.useState('')
+    const [term, setTerm] = React.useState('programming')
+    const [results, setResults] = React.useState([])
+    console.log(results)
 
     React.useEffect(() => {
-        console.log('ran code!!')
-    }, [])
+        
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', { 
+                params: { 
+                    action: 'query', 
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term
+                }
+            })
+            setResults(data.query.search)
+        }
+
+        
+            search()
+    
+
+    }, [term])
 
     return (
         <div>
